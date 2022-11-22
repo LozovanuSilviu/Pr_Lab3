aafff6b926ab53f01458e311223f503e2f4dee30
Lozovanu Silviu <silviu.lozovanu@codefactorygroup.com>
2022-11-23 00:27:13 +0200

    Finished minimal acceptance criteria for laboratory work nr 3

---
+++ Lab3_Pr/Controllers/DataController.cs
@@ -0,0 +1,60 @@
+﻿using Lab3_Pr.Models;
+using Lab3_Pr.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3_Pr.Controllers;
+
+[ApiController]
+[Route("api")]
+public class DataController : ControllerBase
+{
+    private readonly DataService _service;
+
+    public DataController(DataService service)
+    {
+        _service = service;
+    }
+    
+    [HttpGet("get-by-id/{id}") ]
+    
+    public IActionResult GetById(Guid id)
+    {
+        var element = _service.GetById(id);
+        return Ok(element);
+    }
+    
+    [HttpGet("get-all-elements")]
+    public IActionResult GetAllElements()
+    {
+        var elements = _service.GetAll();
+        return Ok(elements);
+    }
+    
+    [HttpPost("add-new-data")]
+    public IActionResult AddData(CountryCapitalModel model)
+    {
+       var element= _service.Add(model);
+        return Ok(element);
+    }
+    
+    [HttpPut("update-element-by-id/{id}")]
+    public IActionResult UpdateById(Guid id,CountryCapitalModel updated)
+    {
+        _service.Update(id, updated);
+        return Ok();
+    }
+    
+    [HttpDelete("delete-by-id/{id}")]
+    public IActionResult DeleteById(Guid id)
+    {
+        _service.DeleteById(id);
+        return Ok();
+    }
+    
+    [HttpDelete("delete-all-elements")]
+    public IActionResult DeleteAllElements()
+    {
+        _service.DeleteAll();
+        return Ok();
+    }
+}
---
+++ Lab3_Pr/Models/CountryCapital.cs
@@ -0,0 +1,8 @@
+﻿namespace Lab3_Pr.Models;
+
+public class CountryCapital
+{
+    public Guid Id { get; set; }
+    public string CountryName { get; set; }
+    public string CapitalName { get; set; }
+}
